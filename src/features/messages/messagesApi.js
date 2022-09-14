b6e1b74c7140217321f1bba26090ab1845b3b5dc
ferdous <hassanferdous101@gmail.com>
2022-09-14 18:53:35 +0600

    init

---
+++ src/features/messages/messagesApi.js
@@ -0,0 +1,42 @@
+import { io } from 'socket.io-client';
+import { apiSlice } from '../api/apiSlice';
+
+export const messagesApi = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		getMessages: builder.query({
+			query: (id) =>
+				`/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
+			async onCacheEntryAdded(arg, { updateCachedData, cacheDataLoaded, cacheEntryRemoved }) {
+				const socket = io('http://localhost:9000', {
+					reconnectionDelay: 1000,
+					reconnection: true,
+					reconnectionAttemps: 10,
+					transports: ['websocket'],
+					agent: false,
+					upgrade: false,
+					rejectUnauthorized: false,
+				});
+
+				try {
+					await cacheDataLoaded;
+					socket.on('message', ({ message }) => {
+						updateCachedData((draft) => {
+							draft.push(message);
+						});
+					});
+				} catch (error) {
+					await cacheEntryRemoved;
+				}
+			},
+		}),
+		addMessage: builder.mutation({
+			query: (data) => ({
+				url: '/messages',
+				method: 'POST',
+				body: data,
+			}),
+		}),
+	}),
+});
+
+export const { useGetMessagesQuery, useAddMessageMutation } = messagesApi;
